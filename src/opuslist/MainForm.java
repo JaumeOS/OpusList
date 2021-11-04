c5d459668d33e4a1e497926a81095bed304485cb
JaumeOS <77398566+JaumeOS@users.noreply.github.com>
2021-11-04 19:01:27 +0100

    Implementados botones "Insert", "Save" i "View Image"
    Falta revisar DefaulListModel del botón "Delete"

--- src/opuslist/MainForm.java
+++ src/opuslist/MainForm.java
@@ -1,15 +1,29 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package opuslist;
 
-/**
- *
- * @author Alumne
- */
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import com.google.gson.stream.JsonReader;
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+import javax.imageio.ImageIO;
+import javax.swing.DefaultListModel;
+import javax.swing.ImageIcon;
+
 public class MainForm extends javax.swing.JFrame {
+    private static final java.lang.reflect.Type LIST_OF_OBRA_TYPE = new TypeToken<List<Obra>>() {}.getType();
+    public static ArrayList<Obra> obras = new ArrayList<>();
+
+    private boolean confirmSave = false;
+    private boolean dataChanged = false;
 
     /**
      * Creates new form MainForm
@@ -27,21 +41,278 @@ public class MainForm extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jMenuItem1 = new javax.swing.JMenuItem();
+        btnRead = new javax.swing.JButton();
+        btnUpdate = new javax.swing.JButton();
+        btnDelete = new javax.swing.JButton();
+        btnInsert = new javax.swing.JButton();
+        btnSave = new javax.swing.JButton();
+        lblImage = new javax.swing.JLabel();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        lstObras = new javax.swing.JList<>();
+        txtImagePath = new javax.swing.JTextField();
+        mnuMenuBar = new javax.swing.JMenuBar();
+        mnuCRUD = new javax.swing.JMenu();
+        mniRead = new javax.swing.JMenuItem();
+        mniInsert = new javax.swing.JMenuItem();
+        mniUpdate = new javax.swing.JMenuItem();
+        mniDelette = new javax.swing.JMenuItem();
+
+        jMenuItem1.setText("jMenuItem1");
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setTitle("Opus List");
+        addWindowListener(new java.awt.event.WindowAdapter() {
+            public void windowOpened(java.awt.event.WindowEvent evt) {
+                formWindowOpened(evt);
+            }
+        });
+
+        btnRead.setText("Read");
+        btnRead.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnReadActionPerformed(evt);
+            }
+        });
+
+        btnUpdate.setText("Update");
+
+        btnDelete.setText("Delete");
+        btnDelete.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnDeleteActionPerformed(evt);
+            }
+        });
+
+        btnInsert.setText("Insert");
+        btnInsert.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnInsertActionPerformed(evt);
+            }
+        });
+
+        btnSave.setText("Save");
+        btnSave.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnSaveActionPerformed(evt);
+            }
+        });
+
+        lblImage.setText("Image preview");
+
+        lstObras.addListSelectionListener(new javax.swing.event.ListSelectionListener() {
+            public void valueChanged(javax.swing.event.ListSelectionEvent evt) {
+                lstObrasValueChanged(evt);
+            }
+        });
+        jScrollPane1.setViewportView(lstObras);
+
+        txtImagePath.setText("Image Path");
+        txtImagePath.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                txtImagePathActionPerformed(evt);
+            }
+        });
+
+        mnuCRUD.setText("CRUD");
+        mnuCRUD.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                mnuCRUDActionPerformed(evt);
+            }
+        });
+
+        mniRead.setText("Read");
+        mniRead.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                mniReadActionPerformed(evt);
+            }
+        });
+        mnuCRUD.add(mniRead);
+
+        mniInsert.setText("Insert");
+        mniInsert.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                mniInsertActionPerformed(evt);
+            }
+        });
+        mnuCRUD.add(mniInsert);
+
+        mniUpdate.setText("Update");
+        mniUpdate.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                mniUpdateActionPerformed(evt);
+            }
+        });
+        mnuCRUD.add(mniUpdate);
+
+        mniDelette.setText("Delete");
+        mniDelette.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                mniDeletteActionPerformed(evt);
+            }
+        });
+        mnuCRUD.add(mniDelette);
+
+        mnuMenuBar.add(mnuCRUD);
+
+        setJMenuBar(mnuMenuBar);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(btnInsert, javax.swing.GroupLayout.PREFERRED_SIZE, 73, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(btnSave, javax.swing.GroupLayout.PREFERRED_SIZE, 73, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(btnDelete, javax.swing.GroupLayout.PREFERRED_SIZE, 73, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(btnUpdate, javax.swing.GroupLayout.PREFERRED_SIZE, 73, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(btnRead, javax.swing.GroupLayout.PREFERRED_SIZE, 73, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 484, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(30, 30, 30)
+                        .addComponent(lblImage, javax.swing.GroupLayout.DEFAULT_SIZE, 207, Short.MAX_VALUE)
+                        .addGap(26, 26, 26))
+                    .addGroup(layout.createSequentialGroup()
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addComponent(txtImagePath)
+                        .addContainerGap())))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(55, 55, 55)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(btnInsert)
+                    .addComponent(btnRead)
+                    .addComponent(btnUpdate)
+                    .addComponent(btnDelete)
+                    .addComponent(btnSave))
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(0, 76, Short.MAX_VALUE)
+                        .addComponent(lblImage, javax.swing.GroupLayout.PREFERRED_SIZE, 181, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(113, 113, 113)
+                        .addComponent(txtImagePath, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(jScrollPane1))
+                .addContainerGap())
         );
 
         pack();
+        setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
+
+    private void mnuCRUDActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_mnuCRUDActionPerformed
+
+    }//GEN-LAST:event_mnuCRUDActionPerformed
+
+    private void btnReadActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnReadActionPerformed
+        Gson gson = new Gson();
+        try {
+            JsonReader reader = new JsonReader(new FileReader(System.getProperty("user.home") + "\\AppData\\Local\\OpusList\\data\\obres.json"));
+            obras = gson.fromJson(reader, LIST_OF_OBRA_TYPE);
+
+            DefaultListModel obrasListModel = new DefaultListModel();
+            for (Obra o : obras) {
+                obrasListModel.addElement(o.toString() + System.lineSeparator());
+            }
+            lstObras.setModel(obrasListModel);
+        } catch (FileNotFoundException fnfe) {
+            fnfe.printStackTrace();
+        }
+    }//GEN-LAST:event_btnReadActionPerformed
+
+    private void mniReadActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_mniReadActionPerformed
+        btnRead.doClick();
+    }//GEN-LAST:event_mniReadActionPerformed
+
+    private void mniDeletteActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_mniDeletteActionPerformed
+        btnDelete.doClick();
+    }//GEN-LAST:event_mniDeletteActionPerformed
+
+    private void mniUpdateActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_mniUpdateActionPerformed
+        btnUpdate.doClick();
+    }//GEN-LAST:event_mniUpdateActionPerformed
+
+    private void btnInsertActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnInsertActionPerformed
+        InsertDialog saveDialog = new InsertDialog(this, true);
+        saveDialog.setVisible(true);
+
+//        ConfirmSaveDialog saveDialog = new ConfirmSaveDialog(this, true);
+//        saveDialog.setVisible(true);
+//
+    }//GEN-LAST:event_btnInsertActionPerformed
+
+    private void btnSaveActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnSaveActionPerformed
+
+    }//GEN-LAST:event_btnSaveActionPerformed
+
+    private void lstObrasValueChanged(javax.swing.event.ListSelectionEvent evt) {//GEN-FIRST:event_lstObrasValueChanged
+        String obraName = lstObras.getSelectedValue();
+        String[] splitter = obraName.split(", ");
+        String imageName = splitter[2];
+        try {
+            BufferedImage bufferedImage = ImageIO.read(new File(System.getProperty("user.home") + "\\AppData\\Local\\OpusList\\images\\" + imageName));
+            ImageIcon icon = resizeImageIcon(bufferedImage, lblImage.getWidth(), lblImage.getHeight());
+            lblImage.setIcon(icon);
+            txtImagePath.setText(System.getProperty("user.home") + "\\AppData\\Local\\OpusList\\images\\" + imageName);
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        }
+    }//GEN-LAST:event_lstObrasValueChanged
+
+    private void txtImagePathActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_txtImagePathActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_txtImagePathActionPerformed
+
+    private void formWindowOpened(java.awt.event.WindowEvent evt) {//GEN-FIRST:event_formWindowOpened
+        btnRead.doClick();
+    }//GEN-LAST:event_formWindowOpened
+
+    private void mniInsertActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_mniInsertActionPerformed
+        btnInsert.doClick();
+    }//GEN-LAST:event_mniInsertActionPerformed
+
+    private void btnDeleteActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnDeleteActionPerformed
+        User selectedUser = lstUsers.getSelectedValue();
+        users.remove(selectedUser);
+
+        
+        dataChanged = true;
+    }//GEN-LAST:event_btnDeleteActionPerformed
+
+    ImageIcon resizeImageIcon(BufferedImage originalImage, int desiredWidth, int desiredHeight) {
+        int newHeight;
+        int newWidth;
+        float aspectRatio = (float) originalImage.getWidth() / (float) originalImage.getHeight();
+        if (originalImage.getWidth() > originalImage.getHeight()) {
+            newWidth = desiredWidth;
+            newHeight = Math.round(desiredWidth / aspectRatio);
+        } else {
+            newHeight = desiredHeight;
+            newWidth = Math.round(desiredHeight * aspectRatio);
+        }
+        Image resultingImage = originalImage.getScaledInstance(newWidth, newHeight, Image.SCALE_SMOOTH);
+        BufferedImage outputImage = new BufferedImage(newWidth, newHeight, BufferedImage.TYPE_INT_RGB);
+        outputImage.getGraphics().drawImage(resultingImage, 0, 0, null);
+        ImageIcon imageIcon = new ImageIcon(outputImage);
+        return imageIcon;
+    }
+
+    public boolean isConfirmSave() {
+        return confirmSave;
+    }
+
+    public void setConfirmSave(boolean confirmSave) {
+        this.confirmSave = confirmSave;
+    }
 
     /**
      * @param args the command line arguments
@@ -79,5 +350,21 @@ public class MainForm extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btnDelete;
+    private javax.swing.JButton btnInsert;
+    private javax.swing.JButton btnRead;
+    private javax.swing.JButton btnSave;
+    private javax.swing.JButton btnUpdate;
+    private javax.swing.JMenuItem jMenuItem1;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JLabel lblImage;
+    private javax.swing.JList<String> lstObras;
+    private javax.swing.JMenuItem mniDelette;
+    private javax.swing.JMenuItem mniInsert;
+    private javax.swing.JMenuItem mniRead;
+    private javax.swing.JMenuItem mniUpdate;
+    private javax.swing.JMenu mnuCRUD;
+    private javax.swing.JMenuBar mnuMenuBar;
+    private javax.swing.JTextField txtImagePath;
     // End of variables declaration//GEN-END:variables
 }
